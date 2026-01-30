04c22d5ad036ca96c7e57692a7df83511f254de8
Program <ProgramCX@Outlook.com>
2026-01-30 09:24:26 +0800

    添加ESLint配置、Git忽略文件和MIT许可证

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,47 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import pluginVue from 'eslint-plugin-vue'
+import pluginJs from '@eslint/js'
 import tseslint from '@typescript-eslint/eslint-plugin'
-import tseslintParser from '@typescript-eslint/parser'
+import tsEslint from '@typescript-eslint/parser'
+import vueEslint from 'eslint-plugin-vue'
 
 export default [
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
   {
-    files: ['**/*.{js,ts,vue}'],
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  {
+    files: ['**/*.{js,ts}'],
     languageOptions: {
-      parser: tseslintParser,
       globals: {
         ...globals.browser,
         ...globals.node
+      },
+      parser: tsEslint,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
       }
     },
     plugins: {
-      vue: pluginVue,
-      '@typescript-eslint': tseslint
+      '@typescript-eslint': tseslint,
+      vue: vueEslint
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...vueEslint.configs['flat/essential'].rules,
+      
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      
+      // Vue
       'vue/multi-word-component-names': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
+      'vue/no-v-html': 'warn',
+      
+      // General
+      'no-console': 'warn',
+      'no-debugger': 'error'
     }
   }
 ]
